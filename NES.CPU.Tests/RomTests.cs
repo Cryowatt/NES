2d0c16d02d012f961c64a8c557235268d03b4e4a
Eric Carter <eric.carter@cryowatt.com>
2019-11-20 21:11:27 -0800

    Adding test roms Adding start of RomImage class

---
+++ NES.CPU.Tests/RomTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace NES.CPU.Tests
+{
+    public class RomTests
+    {
+        [Fact]
+        public void Test()
+        {
+            using (var reader = new BinaryReader(File.OpenRead("TestRoms/01-basics.nes")))
+            {
+                var romFile = RomImage.From(reader);
+            }
+        }
+    }
+}
---
+++ NES.CPU/RomImage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NES.CPU
+{
+    public class RomImage
+    {
+        public byte ProgramSize { get; private set; }
+        public byte CharacterSize { get; private set; }
+
+        public static RomImage From(BinaryReader reader)
+        {
+            var header = new string(reader.ReadChars(4));
+            
+            if (header != "NES\x1a")
+            {
+                throw new BadImageFormatException();
+            }
+            
+            var image = new RomImage();
+
+            image.ProgramSize = reader.ReadByte();
+            image.CharacterSize = reader.ReadByte();
+
+            return image;
+        }
+    }
+}
